d5ac1f8a5980c5d07ee83e11ab2f2c617cd29cd3
JyothiBogala-Nodejs <mvnjyothireddy@gmail.com>
2020-08-23 03:30:41 -0600

    Add Registration form.

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -5,4 +5,8 @@ exports.signupForm = function(req, res) {
     res.render('accounts/new');
 };
 
+exports.create = function(req, res) {
+     console.log(req.body)
 
+     res.end
+};
--- routes/accounts.js
+++ routes/accounts.js
@@ -5,6 +5,6 @@ var router = express.Router();
 router 
   .route('/register')
   .get(accountController.signupForm)
-  
+  .post(accountController.create)
 
 module.exports = router;
