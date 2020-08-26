fd5efd3ec2cce08c1b93191fd6496d435f9197af
JyothiBogala-Nodejs <mvnjyothireddy@gmail.com>
2020-08-26 17:01:56 -0600

    Add server side validation

--- models/user.js
+++ models/user.js
@@ -2,30 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Bcrypt = require("bcryptjs");
 
-//Create a schema
 var userSchema = new Schema({
-   username: { 
-      type: String,
-      lowercase: true,
-      trim: true, 
-      required: true
-    },
-   password: { type: String, required: true }
+   username: {
+       type: String,
+       lowercase: true,
+       trim: true,
+       required: [true, 'Username is required.'],
+       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Username must be a valid email address']
+   },
+   password: {
+        type: String, 
+        required: [true, 'Password is required.']
+   }
 },
 {
    timestamps: true
 });
 
 userSchema.pre("save", function(next) {
-   if(!this.isModified("password")) {
-       return next();
-   }
-   this.password = Bcrypt.hashSync(this.password, 10);
-   next();
+    if(!this.isModified("password")) {
+        return next();
+    }
+    this.password = Bcrypt.hashSync(this.password, 10);
+    next();
 });
 
-//We need to create a model using it
 var User = mongoose.model('User', userSchema);
 
-//make this available to our users in our Node applications
 module.exports = User;
